571d56a307d42bdad512797e95711c18b6cfb7e3
malfaifi001-dot <malfaifi001@gmail.com>
2026-06-18 16:50:09 +0300

    Use bundled MariaDB driver for Hostinger build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@prisma/adapter-mariadb", "mariadb"],
+  /* config options here */
 };
 
 export default nextConfig;
