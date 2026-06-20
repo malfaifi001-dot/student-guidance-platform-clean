e692dc95b9b9fd108ce2c8e0918388f5e9c8a82c
malfaifi001-dot <malfaifi001@gmail.com>
2026-06-20 14:28:29 +0300

    app-5 fixed color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type {  Metadata, Viewport } from "next";
 import { Cairo } from "next/font/google";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import "./globals.css";
@@ -9,6 +9,14 @@ const cairo = Cairo({
   display: "swap",
 });
 
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
+};
 export const metadata: Metadata = {
   title: "منصة التوجيه الطلابي",
   description:
